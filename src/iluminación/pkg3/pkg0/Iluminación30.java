7e4d541de4a9ee04747803bce3fd1de16fb77115
Fabio Cortes <fcor14@hotmail.com>
2016-12-01 09:37:30 -0500

    Initial commit. Creación del proyecto con el frame VentanaPrincipal

---
+++ src/iluminación/pkg3/pkg0/Iluminación30.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package iluminación.pkg3.pkg0;
+
+/**
+ *
+ * @author hp
+ */
+public class Iluminación30 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        VentanaPrincipal visual=new VentanaPrincipal();
+        visual.setVisible(true);
+    }
+    
+}
